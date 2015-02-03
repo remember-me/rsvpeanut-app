b83dd1e4217ed2d5e5c62bf0c3224631f3fb69f3
Alex Ford <alexford90@gmail.com>
2015-02-03 16:58:31 +0000

    Get Request on Search Click

--- app/controllers/events.js
+++ app/controllers/events.js
@@ -3,8 +3,14 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     search: function(query){
-      debugger;
-      console.log("Searching for " + query);
+      var store = this.store;
+      store.find('event', { location: query })
+      .then(function(){
+        alert('Requested Events for ' + query + '!');
+      })
+      .catch(function(){
+        alert('Failed to request Events for ' + query + '!');
+      });
     },
   }
 });
--- app/routes/events/event.js
+++ app/routes/events/event.js
@@ -1,7 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function(params) {
-    return this.store.find('event', params.event_id);
-  }
+
 });
