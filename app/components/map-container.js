313e0ce9edf5a5df8ef1f4a9f3d4c4a174bfbea6
Melissa Patterson <melissa.patterson.va@gmail.com>
2015-02-06 13:56:59 -0600

    add zoom to search feature!!!!!!!!!!

--- app/components/map-container.js
+++ app/components/map-container.js
@@ -11,32 +11,43 @@ export default Ember.Component.extend({
     var map = L.mapbox.map('map', 'aford.l4a1dfc2');
     this.set('map', map);
   }.on('didInsertElement'),
-
+  
   dropMarkers: function() {
     var events = this.get('events');
     var map = this.get('map');
+    var geojson = { "type": "FeatureCollection", "features": [] };
     
     // Build All Pin Markers for this new layer
-    var markers = [];
+//     var markers = [];
     events.forEach( function(event){
       var lat = parseFloat(event.get('lat'));
       var long = parseFloat(event.get('long'));
       if( lat && long ){
-        markers.push(L.marker([lat, long]));
+        geojson['features'].push({"type": "Feature", "geometry": {"type": "Point","coordinates": [long,lat]}});
       }
     });
-
+    
+    debugger;
+    var featureLayer = this.buildFeatureLayer(geojson);
+      
+    this.set('layer', featureLayer);
+    
+    map.fitBounds(featureLayer.getBounds());
+            
     console.log("Droping " + events.length + " Events!");
-
+/*
+    
     var newLayer = L.layerGroup(markers); // Get new Current Layer of markers
     this.removeLayer();                   // Remove Previous Filtered Layer
     newLayer.addTo(map);                  // Add Filtered Layer to Map
-/*
-    debugger;
-    map.fitBounds(newLayer.getBounds());
-*/
-    this.set('layer', newLayer);          // Set New Layer in Controller  
+    this.set('layer', newLayer);
+*/          // Set New Layer in Controller  
   }.observes('events.@each'),
+  
+  buildFeatureLayer: function(geojson) {
+    var map = this.get('map');
+    return L.mapbox.featureLayer(geojson).addTo(map);
+  },
   
   removeLayer: function() {
     var currentLayer = this.get('layer');
@@ -45,12 +56,4 @@ export default Ember.Component.extend({
       map.removeLayer(currentLayer);
     }
   }
-  
-/*
-  zoomLayer.on('dropMarkers', function() {
-    // featureLayer.getBounds() returns the corners of the furthest-out markers,
-    // and map.fitBounds() makes sure that the map contains these.
-    map.fitBounds(featureLayer.getBounds());
-  });
-*/
 });
